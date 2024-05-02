6ad1884a4cbbcb3cf114279195c768b8c215a6cb
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-05-02 09:55:53 +0300

    create themes of app and create home page

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac/homepage.dart';
+
+void main() {
+  runApp(const tic_tac());
+}
+
+class tic_tac extends StatelessWidget {
+  const tic_tac({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+          primaryColor:const Color(0xff00061a),
+          shadowColor: const Color(0xff001456),
+          splashColor: const Color(0xff4169e8)
+        ),
+        home: const HomePage(),
+    );
+  }
+}
