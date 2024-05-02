c0c60e692a94318ced312eccc5d9fd3b00632f4f
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-05-02 10:29:40 +0300

    add text and result and buttun to home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tic_tac/logic_game.dart';
@@ -11,31 +10,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String activeplayer = "X";
-  bool gameover = false ;
-  String result = '';
-  int turns = 0 ;
+  bool gameover = false;
+  String result = 'ay 7aga';
+  int turns = 0;
   Game game = Game();
-  bool iSswitch = false ;
+  bool iSswitch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body:  SafeArea(
-        child: Column(children: [
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 1
             SwitchListTile.adaptive(
-              title:
-               const Text("Turn on/off Two Players",
-               textAlign: TextAlign.center,
-               style: TextStyle( 
-                fontSize: 28,color: Colors.white,
-              ),),
-              value: iSswitch ,
-               onChanged:(bool newvalue){
+                title: const Text(
+                  "Turn on/off Two Players",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+                value: iSswitch,
+                onChanged: (bool newvalue) {
                   iSswitch = newvalue;
-            })
-        ],) ,   
+                }),
+
+            // 2
+            Text(
+              "IT'S $activeplayer TURN",
+              style: const TextStyle(
+                fontSize: 52,
+              ),
+            ),
+            // 3
+// ///////////////////////////////////////
+            // 4
+            Text(
+              result,
+              style: const TextStyle(
+                fontSize: 42,
+              ),
+            ),
+            // 5
+            ElevatedButton.icon(
+                onPressed: () {
+                  activeplayer = "X";
+                  gameover = false;
+                  result = '';
+                  turns = 0;
+                },
+                icon: const Icon(Icons.replay),
+                label: const Text("Repeat The Game"))
+          ],
+        ),
       ),
     );
   }
