da17ad317d62ad2e25b2709c1acb486840e2cb8f
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-05-02 20:09:23 +0300

    making app responsive

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:tic_tac/logic_game.dart';
 
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String activeplayer = "X";
   bool gameover = false;
-  String result = 'ay 7aga';
+  String result = '';
   int turns = 0;
   Game game = Game();
   bool iSswitch = false;
@@ -20,108 +21,66 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            // 1
-            SwitchListTile.adaptive(
-                title: const Text(
-                  "Turn on/off Two Players",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
+          child: MediaQuery.of(context).orientation == Orientation.portrait
+              ? Column(
+                  children: [
+                    ...firstBlock(),
+                    _expanded(context),
+                    ...lastblock(),
+                  ],
+                )
+              : Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [...firstBlock(), ...lastblock()],
+                         
+                      ),
+                    ),
+                    _expanded(context),
+                  ],
+                )),
+    );
+  }
+
+  Expanded _expanded(BuildContext context) {
+    return Expanded(
+        child: GridView.count(
+      crossAxisCount: 3,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      childAspectRatio: 1,
+      padding: const EdgeInsets.all(16),
+      children: List.generate(
+          9,
+          (index) => InkWell(
+                onTap: gameover
+                    ? null
+                    : () {
+                        return _ontap(index);
+                      },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).shadowColor,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Center(
+                    child: Text(
+                      player.playerX.contains(index)
+                          ? "X"
+                          : player.playero.contains(index)
+                              ? "O"
+                              : " ",
+                      style: TextStyle(
+                          color: player.playerX.contains(index)
+                              ? Colors.blue
+                              : Colors.pink,
+                          fontSize: 52),
+                    ),
                   ),
                 ),
-                value: iSswitch,
-                onChanged: (bool newvalue) {
-                  iSswitch = newvalue;
-                }),
-
-            // 2
-            Text(
-              "IT'S $activeplayer TURN",
-              style: const TextStyle(
-                fontSize: 52,
-              ),
-            ),
-            // 3
-            Expanded(
-                child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8,
-              childAspectRatio: 1,
-              padding: const EdgeInsets.all(16),
-              children: List.generate(
-                  9,
-                  (index) => InkWell(
-                        onTap: gameover
-                            ? null
-                            : () {
-                                return _ontap(index);
-                              },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).shadowColor,
-                              borderRadius: BorderRadius.circular(16)),
-                          child: Center(
-                            child: Text(
-                              player.playerX.contains(index)
-                                  ? "X"
-                                  : player.playero.contains(index)
-                                      ? "O"
-                                      : " ",
-                              style: TextStyle(
-                                  color: player.playerX.contains(index)
-                                      ? Colors.blue
-                                      : Colors.pink,
-                                  fontSize: 52),
-                            ),
-                          ),
-                        ),
-                      )),
-            )),
-            // 4
-            Text(
-              result,
-              style: const TextStyle(
-                fontSize: 42,
-              ),
-            ),
-            // 5
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: ElevatedButton.icon(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).splashColor),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ))),
-                  onPressed: () {
-                    setState(() {
-                      player.playerX = [];
-                      player.playero = [];
-                      activeplayer = "X";
-                      gameover = false;
-                      result = '';
-                      turns = 0;
-                    });
-                  },
-                  icon: const Icon(
-                    Icons.replay,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    "Repeat The Game",
-                    style: TextStyle(color: Colors.white),
-                  )),
-            )
-          ],
-        ),
-      ),
-    );
+              )),
+    ));
   }
 
   _ontap(int index) async {
@@ -129,7 +88,7 @@ class _HomePageState extends State<HomePage> {
       game.playgame(index, activeplayer);
       UpdateState();
       if (!iSswitch && !gameover && turns != 9) {
-       await game.autoplay(activeplayer);
+        await game.autoplay(activeplayer);
         UpdateState();
       }
     }
@@ -138,14 +97,83 @@ class _HomePageState extends State<HomePage> {
   void UpdateState() {
     return setState(() {
       activeplayer = activeplayer == "X" ? "O" : "X";
-      turns ++ ;
+      turns++;
       String winnerplayer = game.checkwinner();
-      if(winnerplayer != ""){
+      if (winnerplayer != "") {
         gameover = true;
         result = "$winnerplayer is the winner";
-      } else if(!gameover && turns == 9){
+      } else if (!gameover && turns == 9) {
         result = "it's a Draw";
       }
     });
+  }
+
+  List<Widget> firstBlock() {
+    return [
+      SwitchListTile.adaptive(
+          title: const Text(
+            "Turn on/off Two Players",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.white,
+            ),
+          ),
+          value: iSswitch,
+          onChanged: (bool newvalue) {
+            iSswitch = newvalue;
+          }),
+
+      // 2
+      Text(
+        "IT'S $activeplayer TURN",
+        style: const TextStyle(
+          fontSize: 52,
+        ),
+      ),
+    ];
+    // 1
+  }
+
+  List<Widget> lastblock() {
+    return [
+      // 4
+      Text(
+        result,
+        style: const TextStyle(
+          fontSize: 42,
+        ),
+      ),
+      // 5
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ElevatedButton.icon(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).splashColor),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ))),
+            onPressed: () {
+              setState(() {
+                player.playerX = [];
+                player.playero = [];
+                activeplayer = "X";
+                gameover = false;
+                result = '';
+                turns = 0;
+              });
+            },
+            icon: const Icon(
+              Icons.replay,
+              color: Colors.white,
+            ),
+            label: const Text(
+              "Repeat The Game",
+              style: TextStyle(color: Colors.white),
+            )),
+      )
+    ];
   }
 }
